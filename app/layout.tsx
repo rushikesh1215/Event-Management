83fa51601cb03018f538f27e6d94bf5acf72e701
Rushikesh Jaybhaye <jaybhayerushikesh655@gmail.com>
2026-04-17 15:46:37 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Noto_Sans, Montserrat } from "next/font/google";
 import "./globals.css";
 import { NeonAuthUIProvider } from "@neondatabase/auth/react";
 import { authClient } from "@/lib/auth/client";
+import { cn } from "@/lib/utils";
+
+const montserratHeading = Montserrat({subsets:['latin'],variable:'--font-heading'});
+
+const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, montserratHeading.variable)}
     >
       <body  className="min-h-full flex flex-col">
          <NeonAuthUIProvider authClient={authClient as any} defaultTheme="dark">
